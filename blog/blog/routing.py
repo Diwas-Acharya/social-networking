a5cf5d25fdfc16f98a6cd8a20080b7c929c6ed82
Diwas Acharya <47555075+Diwas-Acharya@users.noreply.github.com>
2019-09-29 23:14:29 -0700

    chat room complete

--- blog/blog/routing.py
+++ blog/blog/routing.py
@@ -1,5 +1,20 @@
+<<<<<<< HEAD
+# mysite/routing.py
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import chat.routing
+
+application = ProtocolTypeRouter({
+    # (http->django views is added by default)
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+=======
 from channels.routing import ProtocolTypeRouter
 
 application = ProtocolTypeRouter({
     # Empty for now (http->django views is added by default)
+>>>>>>> cc1c517a828147c4443c82bd127b223322e1666a
 })
--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,3 +1,14 @@
+<<<<<<< HEAD
+from django.http import HttpResponse
+from django.shortcuts import render
+def index(request):
+	return HttpResponse("<script>document.location='account/register/5'</script>")
+
+
+def saurav(request):
+	return render(request,"savrav.html",{})
+=======
 from django.http import HttpResponse
 def index(request):
 	return HttpResponse("<script>document.location='account/register/5'</script>")
+>>>>>>> cc1c517a828147c4443c82bd127b223322e1666a
